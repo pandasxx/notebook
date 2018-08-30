403cb68e5047ae0679f8ffc7b5087d4f05f15384
pandasxx <18512503190@163.com>
2018-08-30 18:08:45 +0800

    m

---
+++ WORK/env/feature_extractor.py
@@ -0,0 +1,68 @@
+from keras.models import *
+from keras.layers import *
+from keras.applications import *
+from keras.preprocessing.image import *
+from keras import optimizers
+from keras.applications.vgg19 import preprocess_input
+from keras.utils.training_utils import multi_gpu_model
+import tensorflow as tf
+import cv2
+
+class FeatureExtrctor(object):
+    """docstring for FeatureExtrctor"""
+    def __init__(self, arg):
+        
+        self.arg = arg
+
+    def build_model(self):
+        with tf.device('/cpu:0'):
+
+        # build base model body
+            input_tensor = Input((224, 224, 3))
+            x = Lambda(vgg19.preprocess_input)(input_tensor)
+        
+            base_model = VGG19(input_tensor=x, weights=None, include_top=False)
+        
+        # build temp model for weights assign
+            m_out = base_model.output
+            flatten = Flatten(name='flatten')(m_out)
+            fc1 = Dense(4096, activation='relu', name='fc1')(flatten)
+            drop_fc1 = Dropout(1.0, name='drop_fc1')(fc1)
+            fc2 = Dense(4096, activation='relu', name='fc2')(drop_fc1)
+            drop_fc2 = Dropout(1.0, name='drop_fc2')(fc2)
+            predictions = Dense(18, activation='softmax', name='predictions')(drop_fc2)
+            
+            model_weights = Model(inputs=base_model.input, outputs=predictions)
+            model_weights.load_weights("vgg19_finetune.h5")
+        
+        # build real model for feature extraction
+            m_out = base_model.output
+            head_pool = AveragePooling2D(pool_size=4, strides=2, padding='valid')(m_out)
+            flatten = Flatten(name='flatten')(head_pool)
+            model = Model(inputs=base_model.input, outputs=flatten)
+        
+            for i in range(23):
+                pretrained_weights = model_weights.get_layer(index=i).get_weights()
+                model.get_layer(index=i).set_weights(pretrained_weights)
+                
+            model.summary()
+        
+        parallel_model = multi_gpu_model(model, gpus=4)
+        parallel_model.summary()
+
+        return parallel_model
+
+    def predict(self):
+        pics = []
+        for i in range(128 * 128):
+        single_pic = cv2.imread(pic_path + "/" + file_list[i])
+        pics.append(single_pic)
+        pics = np.array(pics)
+
+        result_all = parallel_model.predict(pics, batch_size=128, verbose=1)
+        result_all = np.array(result_all)
+        result_all_format = result_all.reshape(128,128,2048)
+
+        return result_all_format
+        
+
